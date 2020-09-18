defb6158b2176fed924a573c1091bfb0a74e5049
Muhammad Rizki <rizkimuhammad2301@gmail.com>
2020-09-18 20:54:07 +0700

    add update & delete in entitas company

--- controller/company.controller.js
+++ controller/company.controller.js
@@ -32,11 +32,42 @@ function readCompany(req, res) {
 }
 
 function updateCompany(req, res) {
-    
+    model.Company.update({
+        name: req.body.name,
+        sector: req.body.sector,
+        city: req.body.city,
+        description: req.body.description,
+        instagram: req.body.instagram,
+        linkedin: req.body.linkedin,
+        image: req.body.image,
+        account: req.body.account
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then( function(result) {
+        res.send('Success Update Data Company')
+        res.json(result)
+    })
+    .catch( function(error) {
+        res.json({error: error})
+    })
 }
 
 function deleteCompany(req, res) {
-
+    model.Company.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then( function(result) {
+        res.send('Success Delete Data Company')
+        res.json(result)
+    })
+    .catch( function(error) {
+        res.json({error: error})
+    })
 }
 
 module.exports = {
--- routes/router.js
+++ routes/router.js
@@ -7,19 +7,19 @@ const company = require('../controller/company.controller')
 
 // DEVELOPER
 router.use('/developers', developers)
-router.get('/developer', developer.readDeveloper) // Read
-router.post('/developer', developer.createDeveloper) // Create
-router.put('/developer', developer.updateDeveloper) // Update
-router.delete('/developer', developer.deleteDeveloper) // Delete
-router.get('/developer', developer.readDeveloperByName) // getDatabyName
-router.get('/developer', developer.readDeveloperBySkill) // getDatabySkill
-router.get('/developer', developer.sortDeveloperByRequest) // sortDatabyName
+router.get('/developer', developer.readDeveloper)                               // Read
+router.post('/developer', developer.createDeveloper)                            // Create
+router.put('/developer', developer.updateDeveloper)                             // Update
+router.delete('/developer', developer.deleteDeveloper)                          // Delete
+router.get('/developer', developer.readDeveloperByName)                         // getDatabyName
+router.get('/developer', developer.readDeveloperBySkill)                        // getDatabySkill
+router.get('/developer', developer.sortDeveloperByRequest)                      // sortDatabyName
 
 // COMPANY
-router.get('/companys', company.readCompany)
-router.post('/companys', company.createCompany)
-router.put('/companys/:id', company.updateCompany)
-router.delete('/companys/:id', company.deleteCompany)
+router.get('/companys', company.readCompany)            // Read
+router.post('/companys', company.createCompany)         // Create
+router.put('/companys/:id', company.updateCompany)      // Update
+router.delete('/companys/:id', company.deleteCompany)   // Delete
 
 
 module.exports = router
