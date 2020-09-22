7a6970c784a57eb38fe4f58c6824f42647c21a52
Muhammad Rizki <rizkimuhammad2301@gmail.com>
2020-09-22 14:44:07 +0700

    add dotenv in project

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
-{
-  "development": {
-    "username": "root",
-    "password": "",
-    "database": "hrsql",
-    "host": "127.0.0.1",
+require('dotenv').config();
+
+module.exports = {
+    "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
